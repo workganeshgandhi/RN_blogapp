50c7fa471e6e2909263f4a1ba6c0729805a0b4be
Ganesh G <50323331+workganeshgandhi@users.noreply.github.com>
2026-04-07 23:34:30 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+import 'react-native-gesture-handler';
+import { AppRegistry } from 'react-native';
+import App from './src/App';
+
+// Register the main component
+AppRegistry.registerComponent('main', () => App);
